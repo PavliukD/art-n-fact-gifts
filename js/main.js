64e8365b7b5dd3d85ebe964816174254a7877f77
Dmitriy <albatrozzz1@gmail.com>
2022-10-09 10:41:48 +0300

    summary

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
 //та штука с графиком дат
 (() => {
-    if (window.innerWidth >= 768) {
+    if (window.innerWidth >= 1000) {
         setInterval(() => {
             let marg = 0
             const els = document.querySelectorAll('.dates-graf-item')
@@ -28,5 +28,22 @@
             const table = document.querySelector('.dates-notif')
             table.style.width = `${(marg * 4) + 32 * 4 + 4}px`
         }, 500)
+        return
     }
+    setInterval(() => {
+            let marg = 0
+            const els = document.querySelectorAll('.dates-graf-item')
+            els.forEach(el => {
+                el.children[0].style.width = getComputedStyle(el).marginTop
+
+            })
+            const last = document.querySelector('.dates-final')
+            inwidth = getComputedStyle(last).marginTop.split('p')
+            width = ((last.clientWidth / 2) + Number(inwidth[0]) - 28)
+            last.children[0].style.width = `${width}px`
+            // const wt = getComputedStyle(els[0]).marginTop.split('p')
+            // marg = Number(wt[0])
+            // const table = document.querySelector('.dates-notif')
+            // table.style.width = `${(marg * 4) + 32 * 4 + 4}px`
+        }, 500)
 })();
